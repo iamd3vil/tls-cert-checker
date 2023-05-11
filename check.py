d68358493febc6e47478c576ad467cc966ae7ff1
Sarat Chandra <me@saratchandra.in>
2023-05-11 23:07:52 +0530

    feat: Add cli args to print selected information.

--- check.py
+++ check.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python3
 
-import ssl
-import socket
+import argparse
 import datetime
+import socket
+import ssl
 import sys
-
-# Check if hostname and port are provided as command line arguments
-if len(sys.argv) != 3:
-    print("Usage: python check.py <hostname> <port>")
-    sys.exit(1)
-
-# Get hostname and port from command line arguments
-hostname = sys.argv[1]
-port = int(sys.argv[2])
-
-# Create an SSL context
-context = ssl.create_default_context()
 
 
 def tuple_to_dict(t):
@@ -27,38 +16,101 @@ def tuple_to_dict(t):
     return d
 
 
-# Create a socket to connect to the server
-with socket.create_connection((hostname, port)) as sock:
-    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-        # Get the server's certificate
-        cert = ssock.getpeercert()
+def print_ssl_info(
+    hostname: str,
+    port: int,
+    print_all: bool,
+    print_cert_expiry: bool,
+    print_issue_date: bool,
+    print_days_to_expiry: bool,
+    print_tls_version: bool,
+    print_ciphers_used: bool,
+    print_issuer_info: bool,
+):
+    # Create an SSL context
+    context = ssl.create_default_context()
 
-        # Extract the expiration date from the certificate
-        exp_date = datetime.datetime.strptime(cert["notAfter"], "%b %d %H:%M:%S %Y %Z")
-        issue_date = datetime.datetime.strptime(
-            cert["notBefore"], "%b %d %H:%M:%S %Y %Z"
-        )
+    # Create a socket to connect to the server
+    with socket.create_connection((hostname, port)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            # Get the server's certificate
+            cert = ssock.getpeercert()
 
-        # Get the certificate issuer
-        issuer_list = tuple_to_dict(cert["issuer"])
-        org_name = issuer_list["organizationName"]
-        country_name = issuer_list["countryName"]
-        common_name = issuer_list["commonName"]
+            # Extract the expiration and issuance dates from the certificate
+            exp_date = datetime.datetime.strptime(
+                cert["notAfter"], "%b %d %H:%M:%S %Y %Z"
+            )
+            issue_date = datetime.datetime.strptime(
+                cert["notBefore"], "%b %d %H:%M:%S %Y %Z"
+            )
 
-        # Get the current date and time
-        now = datetime.datetime.now()
+            # Get the certificate issuer
+            issuer_list = tuple_to_dict(cert["issuer"])
+            org_name = issuer_list["organizationName"]
+            country_name = issuer_list["countryName"]
+            common_name = issuer_list["commonName"]
 
-        # Calculate the number of days until the certificate expires
-        days_until_expiry = (exp_date - now).days
+            # Get the current date and time
+            now = datetime.datetime.now()
 
-        # Get the TLS version and ciphers used
-        tls_version = ssock.version()
-        ciphers = ssock.cipher()
+            # Calculate the number of days until the certificate expires
+            days_until_expiry = (exp_date - now).days
 
-        # Print the certificate's expiration date, days until expiry, TLS version, ciphers used, and issuer
-        print("Certificate was issued on:", issue_date)
-        print("Certificate expires on:", exp_date)
-        print("Days until certificate expiry:", days_until_expiry)
-        print("TLS version:", tls_version)
-        print("Ciphers used:", ciphers)
-        print(f"Issuer: {org_name}, {common_name}, {country_name}")
+            # Get the TLS version and ciphers used
+            tls_version = ssock.version()
+            ciphers = ssock.cipher()
+
+            # Print the certificate info based on the provided command-line arguments
+            if print_all or print_cert_expiry:
+                print("Certificate expires on:", exp_date)
+            if print_all or print_issue_date:
+                print("Certificate was issued on:", issue_date)
+            if print_all or print_days_to_expiry:
+                print("Days until certificate expiry:", days_until_expiry)
+            if print_all or print_tls_version:
+                print("TLS version:", tls_version)
+            if print_all or print_ciphers_used:
+                print("Ciphers used:", ciphers)
+            if print_all or print_issuer_info:
+                print(f"Issuer: {org_name}, {common_name}, {country_name}")
+
+
+if __name__ == "__main__":
+    # Define command-line arguments
+    parser = argparse.ArgumentParser(description="Check SSL/TLS certificate info")
+    parser.add_argument("hostname", type=str, help="hostname of the server to check")
+    parser.add_argument("port", type=int, help="port number of the server to check")
+    parser.add_argument("--all", action="store_true", help="print all information")
+    parser.add_argument(
+        "--expiry", action="store_true", help="print certificate expiry date"
+    )
+    parser.add_argument(
+        "--issue", action="store_true", help="print certificate issuance date"
+    )
+    parser.add_argument(
+        "--days", action="store_true", help="print days until certificate expiry date"
+    )
+    parser.add_argument("--tls", action="store_true", help="print TLS version")
+    parser.add_argument("--ciphers", action="store_true", help="print ciphers used")
+    parser.add_argument(
+        "--issuer", action="store_true", help="print certificate issuer information"
+    )
+
+    args = parser.parse_args()
+
+    if not any(
+        [args.expiry, args.issue, args.days, args.tls, args.ciphers, args.issuer]
+    ):
+        args.all = True
+
+    print_ssl_info(
+        args.hostname,
+        args.port,
+        args.all,
+        args.expiry,
+        args.issue,
+        args.days,
+        args.tls,
+        args.ciphers,
+        args.issuer,
+    )
